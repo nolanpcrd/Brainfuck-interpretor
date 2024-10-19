b1dc46429879778d4f751bc1560dd78dbe23a6b3
nolanpcrd <81513481+nolanpcrd@users.noreply.github.com>
2024-10-19 17:57:24 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+window.onload=initListeners;
+
+function initListeners(){
+  document.getElementById('bf').onchange = translateBf;
+}
+
+function translateBf(){
+  let code = document.getElementById('bf').value;
+  let memory = [];
+  let result = "";
+  let currentIndex = 0;
+
+  for (let i=0;i<code.length;i++){
+    let instruction = code.charAt(i);
+    switch (instruction){
+      case '>':
+        currentIndex++;
+        break;
+      case '<':
+        currentIndex--
+        break;
+      case '+':
+        if (typeof memory[currentIndex] !== undefined){
+          memory[currentIndex]++;
+        }
+      case '-':
+        if (typeof memory[currentIndex] !== undefined){
+          memory[currentIndex]--;
+        }
+        else {
+          memory[currentIndex] = 1;
+        }
+        break;
+      case '.':
+        if (typeof memory[currentIndex] !== undefined && memory[currentIndex] >= 1){
+          result += String.fromCharCode(memory[currentIndex]);
+        }
+        else {
+          result = 'error';
+          document.getElementById('result').value = result;
+          return;
+        }
+        break;
+      case '[':
+        break;
+      case ']':
+        break;
+    }
+  }
+  
+  document.getElementById('result').value = result;
+}
